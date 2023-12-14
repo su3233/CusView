3039e7b061f06d8acde46d4c3752c0fac9e7f201
tongsheng.su <mr_su3200@sina.com>
2023-12-15 00:30:22 +0800

    AnimatorSet使用with、after、before

--- app/src/main/java/com/example/cusview/views/cap3_value_animator/ObjectAnimatorFragment.kt
+++ app/src/main/java/com/example/cusview/views/cap3_value_animator/ObjectAnimatorFragment.kt
@@ -1,9 +1,11 @@
 package com.example.cusview.views.cap3_value_animator
 
+import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.core.animation.addListener
 import androidx.viewbinding.ViewBinding
 import com.example.cusview.base.BaseFragment
 import com.example.cusview.databinding.FragmentObjectAnimatiorBinding
@@ -13,6 +15,7 @@ import com.example.cusview.databinding.FragmentObjectAnimatiorBinding
  * 插值器，估值器
  */
 class ObjectAnimatorFragment : BaseFragment<FragmentObjectAnimatiorBinding>() {
+    private var animatorSet: AnimatorSet? = null
     override fun initData() {
 
     }
@@ -26,6 +29,9 @@ class ObjectAnimatorFragment : BaseFragment<FragmentObjectAnimatiorBinding>() {
     }
 
     override fun initClick() {
+        /**
+         * AnimatorSet函数中设置的插值器和时间等，会覆盖掉单个动画中的设置
+         */
         binding.btObjectStart.setOnClickListener {
             val alphaAnimator =
                 ObjectAnimator.ofFloat(binding.tvObject, "alpha", 1.0f, 0f, 1.0f).apply {
@@ -62,17 +68,45 @@ class ObjectAnimatorFragment : BaseFragment<FragmentObjectAnimatiorBinding>() {
             /**
              * AnimatorSet：依次播放，一起播放
              */
-            AnimatorSet().apply {
-                playSequentially(
-                    backgroundAnimator,
-                    scaleAnimator,
-                    translateAnimation,
-                    alphaAnimator
-                )
-                duration = 3000
+//            AnimatorSet().apply {
+//                playSequentially(
+//                    backgroundAnimator,
+//                    scaleAnimator,
+//                    translateAnimation,
+//                    alphaAnimator
+//                )
+//                duration = 3000
+//                start()
+//            }
+
+            /**
+             * AnimatorSet.Builder
+             * 可是实现播放完A动画后同时播放B和C动画
+             */
+            animatorSet = AnimatorSet().apply {
+                addListener(object : Animator.AnimatorListener {
+                    override fun onAnimationStart(p0: Animator) {
+
+                    }
+
+                    override fun onAnimationEnd(p0: Animator) {
+                    }
+
+                    override fun onAnimationCancel(p0: Animator) {
+                    }
+
+                    override fun onAnimationRepeat(p0: Animator) {
+                    }
+
+                })
+                play(scaleAnimator).with(alphaAnimator).after(translateAnimation)
+                duration = 2000
                 start()
             }
 
+        }
+        binding.btObjectEnd.setOnClickListener {
+            animatorSet?.cancel()
         }
     }
 
