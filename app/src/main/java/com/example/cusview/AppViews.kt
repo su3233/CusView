85f2628dcc30cd08faa2384c192b5b76bd4b17c9
tongsheng.su <mr_su3200@sina.com>
2023-12-18 23:52:50 +0800

    application

--- app/src/main/java/com/example/cusview/AppViews.kt
+++ app/src/main/java/com/example/cusview/AppViews.kt
@@ -1,6 +1,8 @@
 package com.example.cusview
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 
 /**
  *@autor sts
@@ -10,6 +12,28 @@ import android.app.Application
 class AppViews : Application() {
     override fun onCreate() {
         super.onCreate()
+        registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+            }
 
+            override fun onActivityStarted(p0: Activity) {
+            }
+
+            override fun onActivityResumed(p0: Activity) {
+            }
+
+            override fun onActivityPaused(p0: Activity) {
+            }
+
+            override fun onActivityStopped(p0: Activity) {
+            }
+
+            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+            }
+
+            override fun onActivityDestroyed(p0: Activity) {
+            }
+
+        })
     }
 }
