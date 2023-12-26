8cfe93f8ee3e76ac2e86bb9ccc386a2b64c3aae9
tongsheng.su <mr_su3200@sina.com>
2023-12-27 00:46:31 +0800

    vector动态矢量图动画

--- app/src/main/java/com/example/cusview/views/cap5_animator_up/AnimatorUpActivity.kt
+++ app/src/main/java/com/example/cusview/views/cap5_animator_up/AnimatorUpActivity.kt
@@ -1,5 +1,11 @@
 package com.example.cusview.views.cap5_animator_up
 
+import android.graphics.drawable.Animatable
+import android.os.Build
+import android.view.View.OnFocusChangeListener
+import androidx.annotation.RequiresApi
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import com.example.cusview.R
 import com.example.cusview.base.BaseActivity
 import com.example.cusview.databinding.ActivityAnimatorUpBinding
 
@@ -14,14 +20,29 @@ class AnimatorUpActivity : BaseActivity<ActivityAnimatorUpBinding>() {
 
     override fun initViewBinding() = ActivityAnimatorUpBinding.inflate(layoutInflater)
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun initClick() {
         binding.btAnimatorUpStart.setOnClickListener {
 
         }
-    }
-    override fun initData() {
+        with(binding.animImg) {
+            isFocusable = true
+            isFocusableInTouchMode = true
+            requestFocus()
+            requestFocusFromTouch()
+        }
+        binding.edit.onFocusChangeListener = OnFocusChangeListener { p0, p1 ->
+            if (p1) {
+                AnimatedVectorDrawableCompat.create(this, R.drawable.animated_vecotr_search).apply {
+                    binding.animImg.setImageDrawable(this)
+                    (binding.animImg.drawable as Animatable).start()
+                }
+            }
+        }
     }
 
+    override fun initData() {
+    }
 
 
 }
